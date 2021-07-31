c3bc5568a39f38fafaff3d7ff069978a8d8a67a5
Miron <miron.ribkin98@mail.ru>
2021-08-01 02:01:34 +0300

    Добавил логику на кнопки

--- js.js
+++ js.js
@@ -1,31 +1,40 @@
+const buttons = document.querySelectorAll(".blackButton")
 const contents = document.querySelectorAll(".content")
-const buttons = document.querySelectorAll("button")
-buttons.forEach(button => {
-  button.onclick = function () {
-    if (button.classList.contains("blackButton")) {
-      button.classList.replace("blackButton" , "whiteButton");
-    } else  {
-      button.classList.replace("whiteButton" , "blackButton")
+function changeButtonsColor (clickedButton) {
+  if (clickedButton.classList.contains("whiteButton")) {
+    clickedButton.classList.replace("whiteButton", "blackButton")
+  } else {
+    clickedButton.classList.replace("blackButton", "whiteButton");
+  }
+  for (let i = 0; i<buttons.length; i++ ){
+    let currentButton = buttons[i];
+    if(clickedButton !== currentButton){
+      currentButton.classList.replace("whiteButton" ,"blackButton");
     }
   }
-});
 
-// (test) => { test }
-// function (test) {}
+}
 
-const button1 = document.getElementById("button1")
-const content1 = document.getElementById("content1")
-button1.addEventListener("click", function () {
-  contents.forEach(content =>{
-    content.style.display = "none";
-  })
-  if (button1.classList.contains("whiteButton")) {
-    content1.style.display = "block"
+function hideContents (clickedButton) {
+  let continent = document.getElementById(clickedButton.dataset.contentId)// обратились к дата контенту
+  if(continent.style.display === "block" ){
+    continent.style.display = "none"; //сделали видимым контент
   } else {
-    content1.style.display = "none"
+    continent.style.display = "block"
   }
-
-} )
-
-
-  
+  for (let i = 0; i<contents.length; i++){
+    let ghostContent = contents[i];
+    if(continent !== ghostContent){
+      ghostContent.style.display = "none"; // прячем контент
+    }
+  }
+}
+function onButtonClick(event) {
+  let clickedButton = event.target;
+  changeButtonsColor(clickedButton);
+  hideContents(clickedButton);
+}
+for (let i = 0; i<buttons.length; i++){
+  let currentButton = buttons[i];
+  currentButton.addEventListener("click", onButtonClick);
+}
